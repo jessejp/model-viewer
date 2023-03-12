372e884680a9ed128f5ceb28723deb43579f2000
jessejp <jessepernila@gmail.com>
2023-03-12 13:38:49 +0200

    setup-threejs

--- model-viewer/src/main.ts
+++ model-viewer/src/main.ts
@@ -1,3 +1,24 @@
-import './style.css'
+import "./style.css";
+import * as THREE from "three";
 
-const canvas = document.querySelector('.webgl');
+
+//Scene and Camera initialize
+const elementCanvas = document.createElement('canvas');
+document.body?.appendChild(elementCanvas);
+const canvas = elementCanvas;
+const renderSize = { w: 800, h: 600 };
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, renderSize.w / renderSize.h);
+scene.add(new THREE.AxesHelper());
+
+// Default Cube
+const MaterialBasic = new THREE.MeshBasicMaterial({color: 0xaaaaaa})
+const MeshDefaultCube = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), MaterialBasic)
+scene.add(MeshDefaultCube);
+
+camera.position.z = 3;
+scene.add(camera);
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+renderer.setSize(renderSize.w, renderSize.h);
+
+renderer.render(scene, camera)
